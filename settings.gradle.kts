da873d57d5ba05f53432f6576993e7945423fa81
yagiz erdem <2yagiz004@gmail.com>
2026-06-13 01:15:37 +0300

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Raft-Consensus-Protocol-Simulator"
---
+++ src/main/java/io/github/raft_server/Main.java
@@ -0,0 +1,7 @@
+package io.github.raft_server;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
